a1cd4bd95c5f68c5a8adcaa9701df6eab5c60b17
Maria Lara <74436232+maria-lara@users.noreply.github.com>
2021-03-17 20:52:42 -0700

    moved seeds over and beginning connection

--- schema.sql
+++ schema.sql
@@ -1,7 +1,35 @@
+
+--CREATE DATABASE--
 DROP DATABASE IF EXISTS employee_trackerDB;
 CREATE DATABASE employee_trackerDB;
 USE employee_trackerDB;
 
+--DEPARTMENT TABLE--
+CREATE TABLE department(
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
+);
+
+--DEPARTMENT TABLE-ROLE---
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL(10,2),
+    department_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+--EMPLOYEE TABLE--
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INTEGER,
+    manager_id INTEGER NULL,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
 
 -- DEPARTMENTS--
 INSERT INTO department (name) VALUES ("Operations");
