2d2f905d66ead4a932daa87728ad4cba6837c697
Maria Lara <74436232+maria-lara@users.noreply.github.com>
2021-03-18 18:04:39 -0700

    updated js file to grab lists

--- index.js
+++ index.js
@@ -2,25 +2,30 @@ const inquirer = require("inquirer")
 const mysql = require("mysql");
 const cTable = require("console.table");
 
+
 let connection = mysql.createConnection({
-  host: "localhost",
+  host: 'localhost',
+  // Your port; if not 3306
   port: 3306,
-  user: "root",
-  password: "Spe8as19!",
-  database: "employee_trackerDB"
+   // Your username
+  user: 'root',
+   // Your passsword
+  password: 'Spe8as19!',
+   // DB name
+  database: 'employee_trackerDB'
 });
 
 // Connection //
 
 connection.connect(function(err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
+    beginPrompt();
 });
 
 
 // Prompt //
 
-function startPrompt() {
+function beginPrompt() {
     inquirer.prompt([
     {
     type: "list",
@@ -69,3 +74,210 @@ function startPrompt() {
 })
 }
 
+
+// 1. View Employees //
+function viewAllEmployees() {
+    connection.query("SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;", 
+    function(err, res) {
+      if (err) throw err
+      console.table(res)
+      beginPrompt()
+  })
+}
+
+// 2. View Employees by Role //
+function viewAllRoles() {
+    connection.query("SELECT employee.first_name, employee.last_name, role.title AS Title FROM employee JOIN role ON employee.role_id = role.id;", 
+    function(err, res) {
+    if (err) throw err
+    console.table(res)
+    beginPrompt()
+    })
+  }
+
+
+// 3. View Employees by Dpt //
+function viewAllDepartments() {
+    connection.query("SELECT employee.first_name, employee.last_name, department.name AS Department FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id;", 
+    function(err, res) {
+      if (err) throw err
+      console.table(res)
+      beginPrompt()
+    })
+  }
+
+  // 4. Add an Employee
+  function addEmployee() { 
+    inquirer.prompt([
+        {
+          name: "firstname",
+          type: "input",
+          message: "Enter their first name:"
+        },
+        {
+          name: "lastname",
+          type: "input",
+          message: "Enter their last name:"
+        },
+        {
+          name: "role",
+          type: "list",
+          message: "What is their role? ",
+          choices: selectRole()
+        },
+        {
+            name: "choice",
+            type: "rawlist",
+            message: "What is their managers name?",
+            choices: selectManager()
+        }
+    ]).then(function (val) {
+      var roleId = selectRole().indexOf(val.role) + 1
+      var managerId = selectManager().indexOf(val.choice) + 1
+      connection.query("INSERT INTO employee SET ?", 
+      {
+          first_name: val.firstName,
+          last_name: val.lastName,
+          manager_id: managerId,
+          role_id: roleId
+          
+      }, function(err){
+          if (err) throw err
+          console.table(val)
+          beginPrompt()
+      })
+
+  })
+}
+
+var roleList = [];
+function selectRole() {
+  connection.query("SELECT * FROM role", function(err, res) {
+    if (err) throw err
+    for (var i = 0; i < res.length; i++) {
+      roleList.push(res[i].title);
+    }
+
+  })
+  return roleList;
+}
+
+var managersList = [];
+function selectManager() {
+  connection.query("SELECT first_name, last_name FROM employee WHERE manager_id IS NULL", function(err, res) {
+    if (err) throw err
+    for (var i = 0; i < res.length; i++) {
+      managersList.push(res[i].first_name);
+    }
+
+  })
+  return managersList;
+}
+
+
+// 5. Update an employee //
+function updateEmployee() {
+    connection.query("SELECT employee.last_name, role.title FROM employee JOIN role ON employee.role_id = role.id;", function(err, res) {
+
+     if (err) throw err
+     console.log(res)
+
+    inquirer.prompt([
+          {
+            name: "lastName",
+            type: "rawlist",
+            choices: function() {
+              var lastName = [];
+              for (var i = 0; i < res.length; i++) {
+                lastName.push(res[i].last_name);
+              }
+              return lastName;
+            },
+            message: "What is this employee's last name? ",
+          },
+          {
+            name: "role",
+            type: "rawlist",
+            message: "What is their new title? ",
+            choices: selectRole()
+          },
+      ]).then(function(val) {
+        var roleId = selectRole().indexOf(val.role) + 1
+        connection.query("UPDATE employee SET WHERE ?", 
+        {
+          last_name: val.lastName
+           
+        }, 
+        {
+          role_id: roleId
+           
+        }, 
+        function(err){
+            if (err) throw err
+            console.table(val)
+            beginPrompt()
+        })
+  
+    });
+  });
+
+  }
+
+  // 6. Add a new role //
+
+  function addRole() { 
+    connection.query("SELECT role.title AS Title, role.salary AS Salary FROM role",   function(err, res) {
+      inquirer.prompt([
+          {
+            name: "Title",
+            type: "input",
+            message: "What is the roles' title?"
+          },
+          {
+            name: "Salary",
+            type: "input",
+            message: "What is the role's salary?"
+  
+          } 
+      ]).then(function(res) {
+          connection.query(
+              "INSERT INTO role SET ?",
+              {
+                title: res.Title,
+                salary: res.Salary,
+              },
+              function(err) {
+                  if (err) throw err
+                  console.table(res);
+                  beginPrompt();
+              }
+          )
+  
+      });
+    });
+    }
+
+//7. Add a new dpt 
+function addDepartment() { 
+
+    inquirer.prompt([
+        {
+          name: "name",
+          type: "input",
+          message: "What is the name of the new department?"
+        }
+    ]).then(function(res) {
+        var query = connection.query(
+            "INSERT INTO department SET ? ",
+            {
+              name: res.name
+            
+            },
+            function(err) {
+                if (err) throw err
+                console.table(res);
+                beginPrompt();
+            }
+        )
+    })
+  }
--- schema.sql
+++ schema.sql
@@ -1,16 +1,16 @@
 
---CREATE DATABASE--
+
 DROP DATABASE IF EXISTS employee_trackerDB;
 CREATE DATABASE employee_trackerDB;
 USE employee_trackerDB;
 
---DEPARTMENT TABLE--
+
 CREATE TABLE department(
     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30)
 );
 
---DEPARTMENT TABLE-ROLE---
+
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30),
@@ -20,7 +20,7 @@ CREATE TABLE role (
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
---EMPLOYEE TABLE--
+
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
     first_name VARCHAR(30),
@@ -31,14 +31,14 @@ CREATE TABLE employee (
     FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
 
--- DEPARTMENTS--
+
 INSERT INTO department (name) VALUES ("Operations");
 INSERT INTO department (name) VALUES ("Sales");
 INSERT INTO department (name) VALUES ("Engineering");
 INSERT INTO department (name) VALUES ("Customer Success");
 INSERT INTO department (name) VALUES ("Finance");
 
--- ROLES --
+
 INSERT INTO role (title, salary, department_id) VALUES ("Chief Operating Officer", 500000, 1);
 INSERT INTO role (title, salary, department_id) VALUES ("Director of Sales", 250000, 2);
 INSERT INTO role (title, salary, department_id) VALUES ("Head of Engineering", 200000, 3);
@@ -46,12 +46,12 @@ INSERT INTO role (title, salary, department_id) VALUES ("Customer Success Manage
 INSERT INTO role (title, salary, department_id) VALUES ("Chief Financial Officer", 180000, 1);
 
 
--- EMPLOYEES --
+
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Amal", "Clooney", 1, null);
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Maria", "Lara", 2, 1);
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Olivia", "Benson", 3, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Olivia", "Benson", 3, 1);
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Stacey", "Abrams", 4, 1);
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Meghan", "Markel", 5, null;
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Meghan", "Markel", 5, null);
 
 
 SELECT * FROM department;
